9c874f1f2bd273fd6551d819c5b9643ebc3c4106
OCOS UK <support@ocos.io>
2025-10-18 14:05:36 +0400

    Add Badge component for UI

---
+++ components/ui/badge.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { cn } from "../../lib/utils";
+
+export function Badge({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        "inline-flex items-center px-2 py-0.5 rounded-md border border-white/15 text-xs font-medium",
+        className
+      )}
+      {...props}
+    />
+  );
+}
